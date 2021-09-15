a3275c5354fb8a322431d2428a02a5df33bfd893
Carlos Efrain Carbonel Del Castillo <u20161b887@upc.edu.pe>
2021-09-15 02:02:19 -0500

    SD1

---
+++ UPC.NoobCryptStore.BE/Base/ResponseBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBEntity
+{ 
+    public class ResponseBase
+    {
+        public bool issuccess { get; set; }
+        public string errocode { get; set; }
+        public string erromessage { get; set; }
+        public object Data { get; set; }
+    }
+}
---
+++ UPC.NoobCryptStore.BE/Model/Category/EntityCategoryBanner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPC.APIBusiness.DBEntity.Model.Category
+{
+    public class EntityCategoryBanner
+    {
+        private int idCategoria;
+        private string descripcion;
+        private string pathImage;
+        private string detalle;
+
+        public int IdCategoria { get => idCategoria; set => idCategoria = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string PathImage { get => pathImage; set => pathImage = value; }
+        public string Detalle { get => detalle; set => detalle = value; }
+    }
+}
